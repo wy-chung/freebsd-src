d7c22046d0f65d47a0a6dc3deb064f71873c4927
Wuyang Chung <wy-chung@outlook.com>
2024-07-27 16:56:19 +0800

    NC

--- sys/riscv/include/pmap.h
+++ sys/riscv/include/pmap.h
@@ -121,8 +121,9 @@ extern vm_offset_t virtual_end;
 	((((va) | (pa)) & L1_OFFSET) == 0 && (size) >= L1_SIZE)
 
 enum _pmap_mode {
-	PMAP_MODE_SV39, // <== this mode
-	PMAP_MODE_SV48,
+	PMAP_MODE_SV39,
+	PMAP_MODE_SV48, // <== set to this mode in pmap_bootstrap()
+	//PMAP_MODE_SV57, // currently not supported
 };
 
 extern enum _pmap_mode pmap_mode;
